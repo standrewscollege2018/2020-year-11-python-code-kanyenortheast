11228757323c0ba38f2514ae4eb91df3ceadd170
kanyenortheast <cke0921@stacmail.net>
2020-02-12 13:40:20 +1300

    Create DTIC.py

---
+++ DTIC.py
@@ -0,0 +1,18 @@
+#DTIC
+#Charlie K
+#11/2/2020
+#Version 1.0
+
+grade = int(input("Enter Grade 0-100:"))
+
+if grade >84 and grade <101:
+    print ("A")
+    print ("B")
+if grade >49 and grade <70:
+    print ("C")
+if grade <50 and grade >-1:
+    print ("F")
+if grade <0:
+    print ("CHEET")
+if grade >100:
+    print ("CHEET")
